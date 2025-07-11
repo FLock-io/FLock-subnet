0f9bd7a250290adc8c99bdecdda73a9c87dbd855
feng <471899214@qq.com>
2025-07-11 14:13:20 +0800

    Change the data of the read chain to hard code

--- neurons/validator.py
+++ neurons/validator.py
@@ -20,14 +20,13 @@ import asyncio
 import torch
 import typing
 import bittensor as bt
-import math
 import numpy as np
 import json
 import hashlib
 from dataclasses import asdict
-
+from flockoff.constants import Competition
 from flockoff import constants
-from flockoff.utils.chain import assert_registered, read_chain_commitment
+from flockoff.utils.chain import assert_registered
 from flockoff.utils.git import check_and_update_code
 from flockoff.validator.chain import (
     retrieve_model_metadata,
@@ -216,24 +215,8 @@ class Validator:
         bt.logging.info(f"Network: {self.config.subtensor.network}")
         bt.logging.info(f"Is testnet: {is_testnet}")
         bt.logging.info("Reading chain commitment")
-        subnet_owner = constants.get_subnet_owner(is_testnet)
 
-        competition = read_chain_commitment(
-            subnet_owner, self.subtensor, self.config.netuid
-        )
-        if competition is None:
-            bt.logging.error("Failed to read competition commitment")
-            return
-
-        comp_dict = asdict(competition)
-        comp_hash = hashlib.sha256(
-            json.dumps(comp_dict, sort_keys=True).encode()
-        ).hexdigest()
-        competition_changed = False
-        if comp_hash != self.last_competition_hash:
-            bt.logging.info(f"Competition hash changed: {comp_hash}")
-            self.last_competition_hash = comp_hash
-            competition_changed = True
+        competition = Competition.from_defaults()
 
         eval_namespace = competition.repo
 
@@ -419,7 +402,7 @@ class Validator:
                 revision = metadata.id.commit
                 last_rev = self.score_db.get_revision(ns)
                 bt.logging.info(f"Metadata namespace: {ns}, commit: {revision}")
-                if not competition_changed and last_rev == revision:
+                if last_rev == revision:
                     bt.logging.info(
                         f"Skipping UID {uid} as it has already been evaluated with revision {revision}"
                     )
