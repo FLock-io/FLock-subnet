a5a6e7843b4dc891b6a098177c245376a097a521
feng <471899214@qq.com>
2025-07-18 09:43:03 +0800

    fix some duplication bug.

--- tests/FlockDataset/utils/test_utils.py
+++ tests/FlockDataset/utils/test_utils.py
@@ -48,12 +48,6 @@ def test_competition_value(node):
     assert (
         comp.minb <= comp.maxb
     ), f"Minb should be less than or equal to maxb, got {comp.minb} > {comp.maxb}"
-    assert (
-        comp.bheight <= comp.maxb
-    ), f"Bheight should be less than or equal to maxb, got {comp.bheight} > {comp.maxb}"
-    assert (
-        comp.bheight >= comp.minb
-    ), f"Bheight should be greater than or equal to minb, got {comp.bheight} < {comp.minb}"
 
     bt.logging.info(
         f"Commitment values: id={comp.id}, repo={comp.repo}, bench={comp.bench}, rows={comp.rows}, pow={comp.pow}"
